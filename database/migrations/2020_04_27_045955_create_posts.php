9dcb82420f395b9a1277b0e3112e08fa29cf49b2
Murodjon <murodbek.0338@gmail.com>
2020-04-28 10:33:00 +0500

    create migration posts and model Post

---
+++ database/migrations/2020_04_27_045955_create_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->notNull();
+            $table->string('short')->notNull();
+            $table->text('content')->notNull();
+            $table->string('img');
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
