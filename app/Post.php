d08b080c1ceecee46b6a5e06a45c0aefd20467b9
Murodjon <murodbek.0338@gmail.com>
2020-05-16 10:18:19 +0500

    Create  scopeMostViews

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,11 @@ class Post extends Model
 {
     public $table = 'posts';
     public $fillable = ['title', 'short', 'content', 'img', 'thumb', 'views'];
+
+    public function scopeMostViews()
+    {
+        return $this->orderBy('views', 'DESC')->limit(4);
+    }
    
    
 }
